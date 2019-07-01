9c79b881133b254b96a605302ce88b54c2e8094c
Tounsi Ghassen <toujours.cssiste@gmail.com>
2019-07-01 12:42:24 +0100

    first commit

---
+++ scratch.py
@@ -0,0 +1,77 @@
+import json
+from tokenization import process_tokens
+
+
+def sblit(ch,c):
+    s=[]
+    for h in ch:
+        for y in h.split(c):
+            if(y!=""):
+                s.append(y)
+    return s
+
+def multisplit(ch,c):
+    s=[]
+    s.append(ch)
+    for k in c:
+        s=sblit(s,k)
+    return s
+
+
+
+with open("entry.json", "r") as read_file:
+    data = json.load(read_file)
+
+#with open("data_file.json", "w") as write_file:
+#    json.dump(data,write_file, indent=4)
+
+
+data=data[1]
+data=data["data"]
+data=data["children"]
+f = open("comments.txt", "w")
+data = data[:-1]
+for d in data:
+    d = d["data"]
+    txt = d["body"]
+    t=multisplit(txt,[" ",",","-","_","/","(",")",":","?",".","[","]","'","|","*","#",";"])
+    for tt in t:
+        try:
+            f.write(tt)
+        except:
+            f.write("")
+
+        f.write("\n")
+f.close()
+
+
+stops=open('stops.txt').read()
+f = open("comments.txt", "r")
+outfile = "cleaned_file.txt"
+fout = open(outfile, "w+")
+stops=multisplit(stops,["\n"])
+for line in f:
+    for word in stops:
+        if(str(line)==str(word+"\n")):
+            line=""
+    fout.write(line)
+fout.close()
+f.close()
+f = open(outfile, "r")
+
+
+fw = open("c1.txt", "w")
+
+#print (stops,"    ")
+(r,b) =process_tokens(f)
+i=-1
+for m1 in r.values():
+    i+=1
+    j=-1
+    for m2 in r.keys():
+            j+=1
+            if(i==j):
+                    zstr=str(m1)+" "+m2
+                    fw.write(zstr)
+
+fw.close()
